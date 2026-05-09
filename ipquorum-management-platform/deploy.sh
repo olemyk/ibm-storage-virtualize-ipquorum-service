d45c5dca3c58236fc881e7bc5f711f6345df8bd1
Ole Kristian Myklebust <Myklebust@no.ibm.com>
2026-05-09 13:01:12 +0200

    chore: Update to version 3.0.6

--- ipquorum-management-platform/deploy.sh
+++ ipquorum-management-platform/deploy.sh
@@ -29,7 +29,7 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Default configuration
-VERSION="${VERSION:-3.0.5}"
+VERSION="${VERSION:-3.0.6}"
 CONTAINER_RUNTIME=""
 COMPOSE_CMD=""
 ENABLE_MONITORING=false
